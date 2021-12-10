d3008191d11560341e72e72bb9f78156f08a41a3
IVC1-5 <IVC1-5@VLAN-25-15-058>
2021-12-10 14:12:38 +0300

    Нашёл ошибку в рле

--- Ex7/RLE.cpp
+++ Ex7/RLE.cpp
@@ -8,62 +8,85 @@ namespace rle {
 
     void static compress(istream& in, ostream& out, bool grouping = false) {
         printf("Compressing...\n");
-        bool isUniquePrev = false;
-        bool isUnique = isUniquePrev;
-        int col = 0;
-        vector<char> chars;
-        for (; !in.eof(); isUniquePrev = isUnique) {
-	        const char ch = in.get();
-            chars.push_back(ch);
-            col++;
-            if (ch != in.peek()) {//UNIQUE
-                isUnique = true;
-                if (!grouping) {
-                    out << col << string(chars.data(), chars.size());
-                    chars.clear();
-                    col = 0;
-                    continue;
+        if (grouping) {
+            int col = 0;
+            vector<char> chars;
+            bool isUniquePrev = false;
+            bool isUnique = isUniquePrev;
+            for (; !in.eof();) {
+                const char ch = in.get();
+                chars.push_back(ch);
+                col++;
+                if (ch != in.peek()) {
+
                 }
-                if (!isUniquePrev) {
-                    out << col << string(chars.data(), chars.size());
-                    chars.clear();
-                    col = 0;
+                else {
+
                 }
-                continue;
-            } else {//NOT UNIQUE
-                isUnique = false;
-                if (!grouping) {
-                    chars.clear();
-                    continue;
-                }
-                if (isUniquePrev) {
-                    if (grouping) out << "-";
-                    out << col << string(chars.data(), chars.size());
-                    chars.clear();
-                    col = 0;
-                }
-                continue;
             }
-            if (chars.size() >= 127 + (isUniquePrev ? 1 : 0)) {
-                if (isUniquePrev) {
+            for (; !in.eof(); isUniquePrev = isUnique) {
+                const char ch = in.get();
+                chars.push_back(ch);
+                col++;
+                if (ch != in.peek()) {//UNIQUE
+                    isUnique = true;
+                    if (!isUniquePrev) {
+                        out << col << string(chars.data(), chars.size());
+                        chars.clear();
+                        col = 0;
+                    }
+                    continue;
+                }
+                else {//NOT UNIQUE
+                    isUnique = false;
+                    if (isUniquePrev) {
+                        out << "-";
+                        out << col << string(chars.data(), chars.size());
+                        chars.clear();
+                        col = 0;
+                    }
+                    continue;
+                }
+                if (chars.size() >= 127 + (isUniquePrev ? 1 : 0)) {
+                    if (isUniquePrev) {
+                        out << "-";
+                    }
+                    out << col << string(chars.data(), chars.size());
+                    chars.clear();
+                    col = 0;
+                    continue;
+                }
+            }
+            if (!chars.empty()) {
+                if (isUnique) {
                     out << "-";
                 }
                 out << col << string(chars.data(), chars.size());
                 chars.clear();
                 col = 0;
-                continue;
             }
         }
-        if (!chars.empty()) {
-            if (isUnique) {
-                out << "-";
+        else {
+            int col = 0;
+            
+            for (; !in.eof();) {
+                const char ch = in.get();
+                col++;
+                
+                if (ch != in.peek()) {
+                    out << col << ch;
+                    col = 0;
+                }
+                if (col >= 127) {
+                    out << col << ch;
+                    col = 0;
+                }
             }
-            out << col << string(chars.data(), chars.size());
-            chars.clear();
-            col = 0;
         }
+        
         printf("Complete!\n");
     }
+
     void static decompress(istream& in, ostream& out) {
         printf("Decompressing...\n");
         int num = 0;
